cb74ab26b78fe56010ee772797a469c0e73a2aed
Albert Doryumu <doryumujnr@gmai.com>
2022-09-08 17:08:36 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,24 +1,2 @@
 #!/bin/bash
-#include <stdio.h>
-
-/**
- * main - prints the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
-
-5 = 
-#include <stdio.h>
-
-/**
- * main - outputs or displays the proper grammer,
- * but the outcome is a piece of art,
- *
- * Return: 0 if exited properly,  otherwise display a non-zer number
- */
-int main(void)
-{
+#include <stdio.h> int main() { puts("\"Programming is like building a multilingual puzzle"); return (0);}
