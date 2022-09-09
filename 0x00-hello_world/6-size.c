b22fca6163bee5a37b4e28212edbb63f2d8dc8b4
Albert Doryumu <doryumujnr@gmai.com>
2022-09-09 07:53:46 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,15 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 
-void main()
+/**
+ *  * main - prints the size of various types
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *   */
+int main(void)
 {
-	clrscr();
-	printf(“TypettttSize (bytes)”);
-	printf(“%d”,sizeof(char));
-	printf(“%d”,sizeof(int));
-	printf(“%d”,sizeof(long int));
-	printf(“%d”,sizeof(float));
-	printf(“%d”,sizeof(double));
-	printf(“%d”,sizeof(long double));
-	getch();
-	return 0
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	return (0)
 }
