a4a138650e9260352d09a0b21b964b1cbddfad8f
Albert Doryumu <doryumujnr@gmai.com>
2022-09-10 11:56:16 -0700

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,33 +1,43 @@
 #include <stdio.h>
 
 /**
- *  * main - prints all possible combinations of two two-digit numbers
- *   * Return: Always 0 (Success)
- *    */
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+*/
+
 int main(void)
 {
-		int i, j;
+	int x, y;
 
-			for (i = 0; i < 100; i++)
-					{
-								for (j = 0; j < 100; j++)
-											{
-															if (i < j)
-																			{
-																								putchar((i / 10) + 48);
-																												putchar((i % 10) + 48);
-																																putchar(' ');
-																																				putchar((j / 10) + 48);
-																																								putchar((j % 10) + 48);
-																																												if (i != 98 || j != 99)
-																																																	{
-																																																							putchar(',');
-																																																												putchar(' ');
-																																																																}
-																																															}
-
-																	}
-									}
-				putchar('\n');
-					return (0);
+	for (x = 0; x < 100; x++)
+	{
+		for (y = 0; y < 100; y++)
+		{
+			if (x < y)
+			{
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
+				putchar(' ');
+				putchar((y / 10) + 48);
+				putchar((y % 10) + 48);
+				if (x != 98 || y != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
