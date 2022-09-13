1173da406db84dca242523f682bc446cd1683794
Albert Doryumu <doryumujnr@gmai.com>
2022-09-13 16:32:59 -0700

    fix-0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - Print "_putchar" followed by a new line.
  * Description: You are not allowed to include standard libraries.
  * Return: 0
- */
+*/
 
 int main(void)
 {
